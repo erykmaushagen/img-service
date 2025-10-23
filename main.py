04a61cc0f42fee6f624b09ae28d843470bd028ed
Eryk M. <eryk.maushagen@gmail.com>
2025-10-23 16:27:02 +0200

    add

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from flask_migrate import Migrate
 from models import db
 import config
 
-app = Flask(__name__)
+app = Flask(__name__) # neue Flask-Anwendung
 
-# PostgreSQL-Verbindung
+# Datenbankverbindung konfigurieren
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"postgresql+psycopg2://{config.db_user_name}:{config.db_user_password}@localhost/{config.db_name}"
 )
 
 
-db.init_app(app)
+db.init_app(app) # db in flask integrieren, verwendet dabei 
 
-with app.app_context():
+with app.app_context(): # tabellen erstellen
     db.create_all()
     print("✅ Tabellen erfolgreich in der Datenbank erstellt.")
